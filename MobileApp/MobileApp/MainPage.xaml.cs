c65c3af25efaf73cd4a59329c1c8b6853b8a94c8
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-07-20 23:55:01 +0530

    readme changed

---
+++ MobileApp/MobileApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace MobileApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BName_Clicked(object sender, EventArgs e)
+        {
+            Preferences.Set("userName",Ent_userName.Text);
+        }
+
+        private void Btn_Retrive_Clicked(object sender, EventArgs e)
+        {
+           var Response= Preferences.Get("userName", string.Empty);
+            Lbl_RetriveName.Text = Response;
+        }
+    }
+}
---
+++ MobileApp/MobileApp/ReadMe.txt
@@ -0,0 +1,9 @@
+﻿xamarin preference 
+	meka use karanne token wage ewwa tiyaganna ewwa database damahama memory eka wadiyen gihin slow wenawa
+	eka hinda preference tiyaganne 4n eka thulamai.
+		dan store karana vidiha balamu eke.mekedi manpage xaml eke tina eka save karanna one 
+		aniwaryen mokada naththam eka xml.cs ekata enne naha.
+	save karapu preference eka ganna button ekak use karanawa.
+		retrive button ekak dala eka allaganna label ekak danawa.eke text kiyala liyanne naha.
+	ehema livvoth eka screen eke wadinawa.eka hinda hodama de thmai karanna text noda inna eka.
+	ita passe preference eken hambena eka label eke txt ekata set karana eka thmai karnne.
