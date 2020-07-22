79f68c37e298bea095ea2d4ef9bfbd6d054dce4d
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-07-22 15:20:46 +0530

    sign up xml created

--- MobileApp/MobileApp/App.xaml.cs
+++ MobileApp/MobileApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MobileApp.pages;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace MobileApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new SignUp();
         }
 
         protected override void OnStart()
---
+++ MobileApp/MobileApp/pages/SignUp.xaml.cs
@@ -0,0 +1,42 @@
+﻿using MobileApp.service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp.pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUp : ContentPage
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnSignUp_Clicked(object sender, EventArgs e)
+        {
+            //check whether password mismathch or not
+            if (!EntPassword.Text.Equals(EntConfirmPassword.Text))
+            {
+                await DisplayAlert("Password Mismatch", "Please Check Your Confirm Password", "Cancel");
+            }
+            else
+            {
+                var response = await ApiService.RegisterUser(EntName.Text, EntEmail.Text, EntPassword.Text);
+                if (response)
+                {
+                    await DisplayAlert("Hi", "Your Account has been Created", "Alright");
+                }
+                else
+                {
+                    await DisplayAlert("Oops", "Something gone Wrong", "Cancel");
+                }
+            }
+        }
+    }
+}
